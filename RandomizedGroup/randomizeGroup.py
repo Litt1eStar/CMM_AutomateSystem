d48f9767449d51ea88007a247ceeb98e9bbf7001
Woody <devkritt@gmail.com>
2025-06-05 11:48:54 +0700

    update : change code structure to use main fn

--- RandomizedGroup/randomizeGroup.py
+++ RandomizedGroup/randomizeGroup.py
@@ -5,22 +5,35 @@ class Student:
         self.studentCode = studentCode
         self.fullName = fullName
         self.nickname = nickname
-    
+        self.mac = 0
+        self.window = 0
+        
     def __str__(self):
-        return f"{self.studentCode} {self.fullName} {self.nickname}"
+        return f"{self.studentCode} {self.fullName} {self.nickname} | Window : {self.window}, Mac : {self.mac}"
 
 def read_from_file(filename):
     df = pd.read_csv(filename, encoding='utf-8')
     return df
 
-df = read_from_file('./studentData.csv')
-rawData = df.values
-students = []
+def convertCSVToUsableData():
+    df = read_from_file('./studentData.csv')
+    rawData = df.values
+    students = []
 
-for i in range(len(rawData)):
-    data = rawData[i]
-    student = Student(data[0], data[1], data[2])
-    students.append(student)
+    for i in range(len(rawData)):
+        data = rawData[i]
+        student = Student(data[0], data[1], data[2])
+        students.append(student)
 
-for i in range(len(students)):
-    print(students[i])
+    for i in range(len(students)):
+        print(students[i])
+    
+    return students
+
+def main():
+    data = convertCSVToUsableData()
+    
+    
+    
+if __name__ == "__main__":
+    main()
